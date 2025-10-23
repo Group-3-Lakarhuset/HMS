65360f89ab279c2329bcf5e59dd43d5ed3e4f98c
Laaasphillip <laaasph@gmail.com>
2025-10-23 14:31:45 +0200

    Added Invoice & Dashboard Razor pages

---
+++ Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace HMS.Services
+{
+    public class UserService
+    {
+        private readonly AuthenticationStateProvider _authStateProvider;
+        private ClaimsPrincipal _cachedUser;
+
+        public UserService(AuthenticationStateProvider authStateProvider)
+        {
+            _authStateProvider = authStateProvider;
+        }
+
+        public async Task<ClaimsPrincipal> GetUserAsync()
+        {
+            if (_cachedUser == null)
+            {
+                var authState = await _authStateProvider.GetAuthenticationStateAsync();
+                _cachedUser = authState.User;
+            }
+
+            return _cachedUser;
+        }
+    }
+}
