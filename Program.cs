66f1cb38854c6b26cb77240dee6dff5303eada2e
Laaasphillip <laaasph@gmail.com>
2025-10-27 14:01:58 +0100

    Added Using Radzen on program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using HMS.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Radzen;
 using YourAppNamespace.Services;
 
 var builder = WebApplication.CreateBuilder(args);
